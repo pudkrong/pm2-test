02dfbc4ef14fb94dbdda97de12225150019e69f7
Pisut Sritrakulchai <pudkrong@hotmail.com>
2015-11-23 02:04:51 +0700

    update version

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var html = 'version 04';
+  var html = 'version 05';
   console.log(html);
   res.send(html);
 });
